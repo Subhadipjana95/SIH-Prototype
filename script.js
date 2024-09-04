6e0e21634d1ab5531759b2449aa06f704ec07867
Subhadip <codesubhadip95@gmail.com>
2024-09-04 12:59:31 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,57 @@
+function loco() {
+    gsap.registerPlugin(ScrollTrigger);
+
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
+
+loco()
+
+function swip() {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 4,
+      spaceBetween: 35,
+      freeMode: true,
+      grabCursor: true,
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+    });
+}
+swip()
+
+gsap.to(".nav1>h1", {
+    scrollTrigger: {
+        trigger: `.nav1>h1`,
+        scroller: `#main`,
+        start: `top -8%`,
+        end: `bottom -14%`,
+        scrub: 0.2,
+        // markers: true
+    },
+    opacity: 0,
+    zIndex: 0
+})
+
+gsap.from(".img-span", {
+    y: 100,
+    duration: 1.5,
+    delay: 0.2
+})
